48f38ff518220b82e481c072fbe95dbe24ceb54c
gaowb <gaowb@123.com>
2021-02-21 18:58:39 +0800

    构建项目，配置全局样式、路由、element-ui

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    redirect: "Login"
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/login/index.vue")
+  }
+];
+const router = new VueRouter({
+  routes
+});
+
+export default router;
